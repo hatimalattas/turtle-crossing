bbe3c167b5f4b653c61bfec27717ec33f950fabe
Hatim Alattas <56390393+hatimalattas@users.noreply.github.com>
2021-02-10 15:53:09 +0300

    Finished Project

--- main.py
+++ main.py
@@ -6,9 +6,38 @@ from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
+screen.bgcolor("black")
 screen.tracer(0)
+
+player = Player()
+player.go_to_start()
+
+car_manager = CarManager()
+scoreboard = Scoreboard()
+
+screen.listen()
+
+screen.onkey(player.move, "Up")
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    car_manager.create_car()
+    car_manager.move_cars()
+
+    # Detect collision
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+
+    # Detect successful crossing
+    if player.is_at_finish():
+        player.go_to_start()
+        scoreboard.level += 1
+        scoreboard.update()
+        car_manager.increase_speed()
+
+
+screen.exitonclick()
--- scoreboard.py
+++ scoreboard.py
@@ -1,5 +1,17 @@
+from turtle import Turtle
 FONT = ("Courier", 24, "normal")
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.color("white")
+        self.penup()
+        self.hideturtle()
+        self.level = 0
+        self.update()
+
+    def update(self):
+        self.clear()
+        self.goto(-200, 250)
+        self.write(f"Level: {self.level}", align="center", font=("Courier", 25, "normal"))
